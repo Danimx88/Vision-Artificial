b2a0e86d19cc9fc21ec911493378fbd25afe71ba
Danimx88 <105736149+Danimx88@users.noreply.github.com>
2022-07-25 00:08:43 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+vector = np.random.randint(1, 10, (500, 2))
+
+
+
+x = list(range(0,100))
+y = list(range(0, 100))
+
+plt.plot(x,y)
+plt.show()
